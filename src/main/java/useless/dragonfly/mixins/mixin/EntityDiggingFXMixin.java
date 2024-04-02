c9011fe6e299933bd1c91d4f78538dcd11249542
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-04-02 11:02:44 -0500

    fix

--- src/main/java/useless/dragonfly/mixins/mixin/EntityDiggingFXMixin.java
+++ src/main/java/useless/dragonfly/mixins/mixin/EntityDiggingFXMixin.java
@@ -10,7 +10,8 @@ import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import useless.dragonfly.model.block.BlockModelDragonFly;
 import useless.dragonfly.registries.TextureRegistry;
 
@@ -22,15 +23,18 @@ public class EntityDiggingFXMixin extends EntityFX {
 	public EntityDiggingFXMixin(World world, double x, double y, double z, double motionX, double motionY, double motionZ) {
 		super(world, x, y, z, motionX, motionY, motionZ);
 	}
-	@Redirect(method = "<init>(Lnet/minecraft/core/world/World;DDDDDDLnet/minecraft/core/block/Block;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/block/Block;getBlockTextureFromSideAndMetadata(Lnet/minecraft/core/util/helper/Side;I)I"))
-	private int particleFromModel(Block instance, Side side, int meta){
-		BlockModel model = BlockModelDispatcher.getInstance().getDispatch(instance);
-		int tex;
-		if (model instanceof BlockModelDragonFly){
-			tex = TextureRegistry.getIndexOrDefault(((BlockModelDragonFly) model).getModelsFromState(block, (int) x, (int) y, (int) z, true)[0].model.getTexture("particle"), block.getBlockTextureFromSideAndMetadata(Side.BOTTOM, meta));
-		} else {
-			tex = block.getBlockTextureFromSideAndMetadata(Side.BOTTOM, meta);
+	@Inject(method = "func_4041_a(III)Lnet/minecraft/client/entity/fx/EntityDiggingFX;", at = @At(value = "HEAD"))
+	private void particleFromModel(int i, int j, int k, CallbackInfoReturnable<EntityDiggingFX> cir){
+		if (world.getBlock(i, j, k) == block){
+			BlockModel model = BlockModelDispatcher.getInstance().getDispatch(block);
+			int meta = world.getBlockMetadata(i, j, k);
+			int tex;
+			if (model instanceof BlockModelDragonFly){
+				tex = TextureRegistry.getIndexOrDefault(((BlockModelDragonFly) model).getModelsFromState(block, i, j, k, true)[0].model.getTexture("particle"), block.getBlockTextureFromSideAndMetadata(Side.BOTTOM, meta));
+			} else {
+				tex = block.getBlockTextureFromSideAndMetadata(Side.BOTTOM, meta);
+			}
+			particleTextureIndex = tex;
 		}
-		return tex;
 	}
 }
